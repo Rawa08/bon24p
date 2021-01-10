b72f017ce4e38a7cd959207417e38b4e1f172721
Rawa Aref <48259778+Rawa08@users.noreply.github.com>
2021-01-10 19:32:21 +0100

    Update settings.py

--- bon/settings.py
+++ bon/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('sk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG')
 
 if DEBUG:
     from env import *
